9420d9de4b84a7a962694b357b533970dc72328f
K RUPESH <KurakulaRupesh1234@outlook.com>
2026-05-15 12:08:38 +0530

    Add files via upload

---
+++ reverse_array.py
@@ -0,0 +1,12 @@
+arr=[1,2,3,4,5,6]
+arr1=sorted(arr,reverse=True)
+print(arr1)
+
+
+from typing import List, Optional
+def reverseArray(nums: List[int]) -> List[int]:
+    arr1=sorted(arr,reverse=True)
+    return arr1
+nums=[1,2,3,4,5,6]
+result=reverseArray(nums)
+print(result)
